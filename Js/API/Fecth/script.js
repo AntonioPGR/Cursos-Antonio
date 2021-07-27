52e9872624308eb858671cfa14eda94bba10bb43
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2021-07-27 13:43:26 -0300

    Fecth api

--- Js/API/Fecth/script.js
+++ Js/API/Fecth/script.js
@@ -1,18 +1,33 @@
 const cep = document.querySelector('#cep')
 
+cep.addEventListener('keyup', (e)=>{
+    if (cep.value.length == 5 && e.keyCode != 8){
+        cep.value += '-'
+    }
+})
+
 cep.addEventListener('blur', (e)=>{
     let search = cep.value.replace('-', '')
+    if (search.length != 8){
+        return
+    }
+
     const options = {
         method: 'GET',
         mode: 'cors',
         cache: 'default'
     }
 
-    fetch(`https://viacep.com.br/ws/${cep}/json/`, options)
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
     .then(response=>{
-        response.json()
-        .then( data =>{
-            console.log(data)
+        response.json().then(data=>{
+            document.querySelector('input#logradouro').value = data.logradouro
+            document.querySelector('input#bairro').value = data.bairro
+            document.querySelector('input#localidade').value = data.localidade
+            document.querySelector('input#uf').value = data.uf
+        })
+        .catch(erro=>{
+            console.log(erro.message)
         })
     })
     .catch((erro)=>{
