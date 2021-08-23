769fdab12da41898b69433a88b882e7d56c3e4ae
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2021-08-23 12:26:00 -0300

    php

---
+++ PHP/Advanced/include/header.php
@@ -0,0 +1,12 @@
+<?php
+echo `
+<header>
+<h1>Titulo principal</h1>
+<h2>este é o cabeçalho</h2>
+<nav>
+    <a href="">link1</a>
+    <a href="">link2</a>
+    <a href="">link3</a>
+</nav>
+</header>
+`;
---
+++ PHP/Advanced/include/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <main>
+        <h1>este é meu conteudo principal</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore, maxime excepturi natus numquam placeat reprehenderit non, quo aspernatur nihil sint tempora dolorem eveniet facere corrupti, aliquam perferendis porro! Praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ab eaque accusantium facere fugiat saepe quia consequuntur iusto excepturi assumenda, autem, tempore nostrum vero qui nam, voluptatum aliquid vitae expedita.</p>
+    </main>
+</body>
+</html>
