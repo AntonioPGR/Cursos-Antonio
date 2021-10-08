b57d24c493a3153afe660c564c387fa2f98252f0
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2021-10-08 12:24:48 -0300

    miniProjects

--- React/app/src/index.js
+++ React/app/src/index.js
@@ -1,82 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Login extends React.Component{
+class Random extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      isloged: false,
-      name: '',
+      currentColor: '',
     }
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.newColor = this.newColor.bind(this)
   }
 
-  handleSubmit(){
-    const validate = (string) => {
-      return !string? false:true;
-    };
-
-    const username = document.querySelector('#username').value;
-    const password = document.querySelector('#password').value;
-
-    console.log(validate(username), validate(password));
-
-    if(validate(username) && validate(password)){
-      this.setState({
-        isloged: true,
-        name: username,
-      })
+  newColor(){
+    function ramdomColor(min, max){
+      return Math.floor(Math.random() * (max - min)) + min;
     }
+
+    const color1 = ramdomColor(0, 256);
+    const color2 = ramdomColor(0, 256);
+    const color3 = ramdomColor(0, 256);
+
+    const color = `rgb(${color1}, ${color2}, ${color3})`;
+    this.setState({
+      currentColor: color,
+    })
+    document.querySelector('body').style.backgroundColor = color
   }
 
   render(){
-    const login = (
-      <form>
-        <p>
-          <label htmlFor="username">Nome de usuario: </label>
-          <input 
-            type="text" 
-            name="username" 
-            id="username" 
-            placeholder="Seu nome aqui" 
-            maxLength="20" 
-          />
-        </p>
-
-        <p>
-          <label htmlFor="password">Senha: </label>
-          <input 
-            type="password" 
-            name="password" 
-            id="password" 
-            placeholder="Sua senha aqui"
-          />
-        </p>
-
-        <p>
-          <input onClick={this.handleSubmit} type="button" value="Login" />
-        </p>
-      </form>
-    );
-
-    const index = (
-      <div>
-        <h1>Hello {this.state.name}!</h1>
-        <p>Welcome to our page!</p>
-      </div>
-    );
-
-    let render = this.state.isloged? index:login;
-
     return(
       <div>
-        {render}
+        <h1>{this.state.currentColor===''?"Click on the button to get a new random color":`your actually color is ${this.state.currentColor}`}</h1>
+        <button onClick={this.newColor}>Change Color</button>
       </div>
-    );
+    )
   }
 }
 
 ReactDOM.render(
-  <Login />,
-  document.querySelector('#root')
-  )
+  <Random />,
+  document.querySelector("#root")
+)
