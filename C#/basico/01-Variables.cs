db49b1fd822ea11c52947e2bb78ef3b379abede2
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2021-10-12 20:13:09 -0300

    C#

---
+++ C#/basico/01-Variables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Run{
+    class Program{
+        static void Main(string[] args){
+            Console.Clear();
+            string Nome = "Antonio";
+            int Idade = 15;
+            char Sexo = 'M';
+            double Altura = 1.82D;
+
+            Console.WriteLine("Olá! meu nome é " + Nome + ", Atualmente tenho " + Idade + " anos, " + Altura + "m de altura e sou do sexo " + Sexo);
+        }
+    }
+}
---
+++ C#/basico/02-Input.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Run{
+    class Program{
+        static void Main(string[] args){
+            Console.Clear();
+
+            Console.Write("Digite seu nome: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Digite o ano de seu nascimento: ");
+            int birthyear = Convert.ToInt32(Console.ReadLine());
+
+            int age = 2021 - birthyear;
+
+            Console.WriteLine("Olá " + username + ", Sua idade é " + age + " anos");
+        }
+    }
+}
