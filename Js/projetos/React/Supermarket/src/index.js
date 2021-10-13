c90bfa1205e149a1f0a3b6c2256ee2f064a7b16d
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2021-10-12 22:19:02 -0300

    updates

---
+++ Js/projetos/React/Supermarket/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SuperMarket from 'classes/SuperMarket';
+
+ReactDOM.render(
+    <SuperMarket />,
+    document.querySelector("#root")
+)
