80c55cc5a2b2004d2aafb8d1d5151b120494e5d7
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2022-06-22 18:47:38 -0300

    FEAT( searchbar ) : variavel com current string - Ao trocar o valor do input, a variavel de cardápio é alterada

--- TypeScript/Alura/React+Ts/static-files/src/pages/cardapio/cardapio.tsx
+++ TypeScript/Alura/React+Ts/static-files/src/pages/cardapio/cardapio.tsx
@@ -2,14 +2,18 @@
 
 // Components
 import { Header } from "components/header";
+import { useState } from "react";
 import { SearchBar } from "./components/searchBar";
 // import { Footer } from "components/footer";
 
 export function Cardapio(){
+  // armazena a string de busca inserida na barra de pesquisa
+  const [searchQuery, setSearchQuery] = useState<string>("")
+
   return (
       <>
         <Header />
-        <SearchBar />
+        <SearchBar onChange={setSearchQuery}/>
       </>
   );
 }
--- TypeScript/Alura/React+Ts/static-files/src/pages/cardapio/components/searchBar/index.tsx
+++ TypeScript/Alura/React+Ts/static-files/src/pages/cardapio/components/searchBar/index.tsx
@@ -1,17 +1,40 @@
 import styles from "./searchBar.module.scss";
-import { ReactComponent as Lupa } from "assets/lupa.svg"
+import { useRef } from "react";
+import { CgSearch } from "react-icons/cg";
 
-export function SearchBar(){
+interface PropsSearchBar{
+  onChange: (query:string) => void
+}
+
+export function SearchBar({ onChange } : PropsSearchBar){
+  // input de busca
+  const searchInput = useRef<HTMLInputElement>(null)
+
+  /*
+   * Pega o valor inserido no input, caso exista, e passa para o componente superior 
+  */
+  const handleChange = () => {
+
+    if(searchInput.current){
+      onChange(searchInput.current.value)
+      console.log(searchInput.current.value)
+    }
+
+  }
+
   return (
     <form className={styles.container}>
       <input 
-      type="text" 
-      name="search" 
-      id="search" 
-      placeholder="Procurando por algo?"
-      autoComplete="off"
-      className={styles.input} />
-      <Lupa className={styles.image} />
+        type="text" 
+        name="search" 
+        id="search" 
+        placeholder="Procurando por algo?"
+        autoComplete="off"
+        className={styles.input} 
+        onChange={handleChange}
+        ref={searchInput}
+      />
+      <CgSearch className={styles.icon} />
     </form>
   )
 }
